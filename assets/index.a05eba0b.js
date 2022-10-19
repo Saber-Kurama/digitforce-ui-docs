7e56a4f5b816d7b90298994663c7c8c5e2172e78
guochao <463561248@qq.com>
2022-10-19 20:59:59 +0800

    deploy

---
+++ assets/index.a05eba0b.js
@@ -0,0 +1,21 @@
+import{E as _}from"./index.ba55b15b.js";import{d as a,o as c,a as p,u as o,j as u,l as t,U as F,v as C,c as h,A as E,t as f,f as e,F as g}from"./index.6bc17152.js";import{C as i,a as r,b as m}from"./index.0905e926.js";import"./_plugin-vue_export-helper.cdc0426e.js";const y=a({__name:"basic",setup(n){return(s,d)=>(c(),p(o(_)))}}),x=C(" No data, please reload! "),B=a({__name:"custom",setup(n){return(s,d)=>(c(),p(o(_),null,{image:u(()=>[t(o(F))]),default:u(()=>[x]),_:1}))}}),b=`<template>
+  <Empty />
+</template>
+
+<script lang="ts" setup>
+import { Empty } from "@dangojs/digitforce-ui";
+<\/script>
+`,A=`<template>
+  <Empty>
+    <template #image>
+      <icon-exclamation-circle-fill />
+    </template>
+    No data, please reload!
+  </Empty>
+</template>
+
+<script lang="ts" setup>
+import { IconExclamationCircleFill } from "@arco-design/web-vue/es/icon";
+import { Empty } from "@dangojs/digitforce-ui";
+<\/script>
+`,v={class:"markdown-body"},D=E('<h3 id="%3Cempty%3E-props" tabindex="-1"><code class="">&lt;empty&gt;</code> Props</h3><div class="md-table"><table><thead><tr><th>\u53C2\u6570\u540D</th><th>\u63CF\u8FF0</th><th>\u7C7B\u578B</th><th style="text-align:center;">\u9ED8\u8BA4\u503C</th></tr></thead><tbody><tr><td>description</td><td>\u63CF\u8FF0\u5185\u5BB9</td><td><code class="">string</code></td><td style="text-align:center;"><code class="">-</code></td></tr><tr><td>img-src</td><td>\u81EA\u5B9A\u4E49\u56FE\u7247\u7684\u5730\u5740</td><td><code class="">string</code></td><td style="text-align:center;"><code class="">-</code></td></tr></tbody></table></div><h3 id="%3Cempty%3E-slots" tabindex="-1"><code class="">&lt;empty&gt;</code> Slots</h3><div class="md-table"><table><thead><tr><th>\u63D2\u69FD\u540D</th><th style="text-align:center;">\u63CF\u8FF0</th><th>\u53C2\u6570</th></tr></thead><tbody><tr><td>image</td><td style="text-align:center;">\u56FE\u7247/\u56FE\u6807</td><td>-</td></tr></tbody></table></div>',4),$=[D],N=a({__name:"api",setup(n,{expose:s}){return s({frontmatter:{},excerpt:void 0}),(d,l)=>(c(),h("div",v,$))}}),k=e("div",{class:"article-header"},[e("h2",{class:"article-header-title"},"\u7A7A\u72B6\u6001 Empty"),e("div",{class:"article-header-desc"},"\u8FD9\u662F\u4E00\u4E2A\u7EC4\u4EF6\u7684\u63CF\u8FF0")],-1),V={class:"article-content"},j=e("h3",null,"\u57FA\u672C\u7528\u6CD5",-1),w=e("p",null,"\u57FA\u672C\u7528\u6CD5\u7684\u63CF\u8FF0",-1),I=e("h3",null,"\u81EA\u5B9A\u4E49\u56FE\u7247\u548C\u6587\u6848",-1),P=e("p",null,"\u901A\u8FC7 image \u63D2\u69FD\u81EA\u5B9A\u4E49\u56FE\u6807\u3001\u56FE\u7247\uFF0C\u6216\u901A\u8FC7\u5185\u5BB9\u4FEE\u6539\u6587\u6848\u3002",-1),S=e("h3",null,"API",-1),G=a({__name:"DemoEmpty",setup(n){return(s,d)=>{const l=f("a-divider");return c(),h(g,null,[k,t(l),e("div",V,[t(m,null,{description:u(()=>[j,w]),default:u(()=>[t(i,null,{default:u(()=>[t(o(y))]),_:1}),t(r,{code:o(b)},null,8,["code"])]),_:1}),t(m,null,{description:u(()=>[I,P]),default:u(()=>[t(i,null,{default:u(()=>[t(o(B))]),_:1}),t(r,{code:o(A)},null,8,["code"])]),_:1}),e("div",null,[S,t(o(N))])])],64)}}});export{G as default};
